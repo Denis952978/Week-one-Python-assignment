1765701bc3bc8c5b1bf7df54ff3310339357b69b
Denis Chisaka <chisakadennis89@gmail.com>
2024-11-06 02:25:13 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+# user_input.py
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+location = input("Enter your location: ")
+
+print(f"Hello {name}, you need to be {age} years old to vote in {location}.")
